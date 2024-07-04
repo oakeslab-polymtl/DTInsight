219d648a560eb7c9083088a8f223c4d7ef96c375
Ryskann <93831988+Ryskann@users.noreply.github.com>
2024-07-04 16:12:48 +0200

    Readable test UI

--- dtui.gd
+++ dtui.gd
@@ -17,17 +17,15 @@ func _on_fuseki_caller_button_enablers_updated():
 func _on_fuseki_caller_button_models_updated():
 	update_node_with(models_container, "model", fuseki_caller.models)
 
-func update_node_with(node, name, content):
+func update_node_with(node, node_name, content):
 	free_all_child(node)
 	for element in content :
 		print(element)
-		var displayed_name: String = element[name]["value"]
+		var displayed_name: String = element[node_name]["value"]
 		displayed_name= displayed_name.split("#")[1]
 		var new_display = generic_display.instantiate()
 		new_display.get_node("Label").text = displayed_name
-		var container = BoxContainer.new()
-		container.add_child(new_display)
-		node.add_child(container)
+		node.add_child(new_display)
 
 func free_all_child(node):
 	for child in node.get_children():
