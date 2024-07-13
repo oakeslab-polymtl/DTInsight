ecb271068e2f2fd02e608befe9a7959f17e59688
Ryskann <93831988+Ryskann@users.noreply.github.com>
2024-07-12 20:11:23 -0400

    Data filter

--- Fuseki/FusekiData.gd
+++ Fuseki/FusekiData.gd
@@ -21,6 +21,20 @@ var model_to_enabler : Array[GenericLinkedNodes]
 var enabler_to_model : Array[GenericLinkedNodes]
 var model : Dictionary
 
+#Data filter
+const unwanted_attributes : Array[String] = [
+	"type",
+	"sameAs",
+	"provides",
+	"providedBy",
+	"enables",
+	"enabledBy",
+	"enables",
+	"hasInput",
+	"inputTo",
+	"fromData"
+]
+
 #Teke a json from a Fuseki query and store the resulting informations in 
 #variables reachable from the Godot application
 func inputDataFromFusekiJSON(json):
@@ -44,7 +58,7 @@ func inputDataFromFusekiJSON(json):
 
 #Parse the json from Fuseki into internal data structure
 #Return an Array of GenericLinkedNodes or a Dictionary depending on the data type
-func parse_fuseki_json(json, is_link = false):
+static func parse_fuseki_json(json, is_link = false):
 	var json_head = json["head"]["vars"]
 	var json_results = json["results"]["bindings"]
 	var result_aggregator: = []
@@ -63,18 +77,18 @@ func parse_fuseki_json(json, is_link = false):
 	return formated_result
 
 #Remove oml link data from values
-func parse_fuseki_value(value) -> String:
+static func parse_fuseki_value(value) -> String:
 	if(value.contains("#")):
 		return value.split("#")[1]
 	return value
 
 #Transform the result agregator in an array of GenericLinkedNodes or
 #in a dictionary depending on the data type
-func format_result(result_aggregator, is_link = false):
+static func format_result(result_aggregator, is_link = false):
 		return parse_link_result(result_aggregator) if (is_link) else parse_element_result(result_aggregator)
 
 #Transform the result agregator in an Array of GenericLinkedNodes
-func parse_link_result(result_agregator) -> Array[GenericLinkedNodes]:
+static func parse_link_result(result_agregator) -> Array[GenericLinkedNodes]:
 	var formated_result : Array[GenericLinkedNodes] = []
 	for result in result_agregator:
 		var link = GenericLinkedNodes.new()
@@ -85,14 +99,20 @@ func parse_link_result(result_agregator) -> Array[GenericLinkedNodes]:
 
 #Transfrom the result agregator in a dictionary, in each entry corresponding 
 #to an elementis a disctionary containing every attributes of this element
-func parse_element_result(result_agregator) -> Dictionary:
+static func parse_element_result(result_agregator) -> Dictionary:
 	var formated_result : Dictionary = {}
 	for result in result_agregator:
 		var entry_name = result[0].json_value
+		if (not formated_result.has(entry_name)):
+			formated_result[entry_name] = {}
+		var attribute_name = result[1].json_value
+		if(attribute_name in unwanted_attributes):
+			continue
+		var attribute_value = result[2].json_value
 		var entry_value : Dictionary = {}
-		entry_value[result[1].json_value] = result[2].json_value
-		if (formated_result.has(entry_name)):
-			formated_result[entry_name].merge(entry_value)
-		else :
-			formated_result[entry_name] = entry_value
+		entry_value[attribute_name] = attribute_value
+		formated_result[entry_name].merge(entry_value)
 	return formated_result
+
+func dump(to_console = false):
+	FusekiDataDumper.dump(self, to_console)
