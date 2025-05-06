515cbeb3c189ebbdaaeda7b85616d4d1c0aabc29
Kérian Fiter <kerian.fiter@gmail.com>
2025-05-06 14:10:18 -0400

    Fixed settings movement when focused

--- MainScene/settings_button.gd
+++ MainScene/settings_button.gd
@@ -3,7 +3,8 @@ extends Button
 func _on_pressed() -> void:
 	#$SettingsPopup.popup()
 	get_node('/root/MainScene/ControlLayer/SettingsBackground').show() 
-
+	CameraSignals.disable_camera_mouvement.emit()
 
 func _on_settings_settings_menu_closed() -> void:
 	get_node('/root/MainScene/ControlLayer/SettingsBackground').hide()
+	CameraSignals.enable_camera_mouvement.emit()
